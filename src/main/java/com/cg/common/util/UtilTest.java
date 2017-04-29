15699feaaea1fad05e9f5c85e4f93112f49d7147
chenggang816 <1305753112@qq.com>
2017-04-29 11:18:35 +0800

    change the main class name

--- src/main/java/com/cg/common/util/UtilTest.java
+++ src/main/java/com/cg/common/util/UtilTest.java
@@ -4,11 +4,11 @@ package com.cg.common.util;
  * Hello world!
  *
  */
-public class App 
+public class UtilTest 
 {
     public static void main( String[] args )
     {
-    	ExecuteHelper.Execute("D:\\gitrepository\\task-schedule-service\\data\\task\\task2\\task.jar");
+    	ExecuteHelper.Execute2("data\\task\\task2\\task.jar",false);
         System.out.println( "Hello World!" );
     }
 }
